7aba2b8133f3159ef4b38c279559c3287a87ab96
AR Arif <dev.ar.arif@gmail.com>
2022-03-25 13:10:18 +0600

    Added 404 page

---
+++ 404.php
@@ -0,0 +1,11 @@
+<?php get_header('header.php')?>
+
+<div class="comon_section_area">
+    <div class="container d-flex justify-content-center align-items-center flex-column"
+        style="width: 100%;min-height: 55vh; gap: 5rem">
+        <h2>Sorry, page is not found</h2>
+        <a class="vmh_button" href="<?php echo site_url('/') ?>">Back to home</a>
+    </div>
+</div>
+
+<?php get_footer('header.php')?>
